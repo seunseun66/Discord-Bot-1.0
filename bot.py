998411828dba075ea0706b46397ec27012b093bd
seunseun66 <oluwaseunakinrodemi@gmail.com>
2026-02-23 01:08:24 -0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,41 @@
+
+import discord
+import requests
+import json
+
+# your meme function
+def get_meme():
+    response = requests.get('https://meme-api.com/gimme')
+    json_data = json.loads(response.text)
+    return json_data['url']
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Logged in as {client.user}!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello World!')
+
+    if message.content.startswith('$meme'):
+        meme_url = get_meme()
+        await message.channel.send(meme_url)
+
+client.run('Bot Token Here')
+
+import requests
+import json
+
+def get_meme():
+  response = requests.get('https://meme-api.com/gimme')
+  json_data = json.loads(response.text)
+  return json_data['url']
